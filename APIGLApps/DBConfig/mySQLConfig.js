c55ddb8f5cbdd9a85be2e8711035bc19cb2dfcaa
miteshpr97 <miteshpradhan97@gmail.com>
2024-05-29 12:33:22 +0530

    add new database

--- APIGLApps/DBConfig/mySQLConfig.js
+++ APIGLApps/DBConfig/mySQLConfig.js
@@ -1,9 +1,9 @@
 const mySqlConfig = {
   db: {
-    user: "root", // sql user
-    password: "server", //sql user password
-    host: "localhost", // if it does not work try- localhost
-    database: "GL_COMMON",
+    user: "admin_globus", // sql user
+    password: "admin@PWD@123", //sql user password
+    host: "103.145.50.213", // if it does not work try- localhost
+    database: "globus_demo",
     connectTimeout: 60000,
     multipleStatements: true,
   },
