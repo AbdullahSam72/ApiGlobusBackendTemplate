bef2ba8f324bf6c159c08854f4f89d0d5e37a3cd
AbdullahSam72 <muhammad.sayeed70@gmail.com>
2024-05-29 12:00:56 +0400

    BY Sayeed

--- APIGLApps/DBCall/SqlServerUtility.js
+++ APIGLApps/DBCall/SqlServerUtility.js
@@ -48,4 +48,39 @@ async function Select_SP(strSP_name, strParameter) {
   }
 }
 
-module.exports = { Select_Query, Select_SP };
+async function Save_SP(strSP_name, strParameter) {
+  try {
+    console.log("strSP_name:" + strSP_name);
+    console.log(config.server + " :: " + config.database);
+    console.log("Parameter:" + strParameter);
+
+    let pool = await sql.connect(config);
+    let reqObj = pool.request();
+
+    if (strParameter != "") {
+      let entries = Object.entries(strParameter).forEach((entry) => {
+        const [key, value] = entry;
+        console.log("Key :" + key);
+        console.log("Values :" + value);
+        reqObj.input(key, value);
+      });
+    }
+    reqObj.output("O_ERR_LVL");
+    reqObj.output("O_ERR_CD");
+    reqObj.output("O_ERR_NM");
+    let resultSet = await reqObj.execute(strSP_name);
+    if (resultSet.output.O_ERR_LVL == 0) {
+      console.log(resultSet.recordsets);
+      return resultSet.recordsets;
+    } else {
+      console.log(resultSet.output.O_ERR_LVL);
+      console.log(resultSet.output.O_ERR_CD);
+      console.log(resultSet.output.O_ERR_NM);
+      return resultSet.output;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { Select_Query, Select_SP, Save_SP };
